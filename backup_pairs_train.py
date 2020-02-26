191926efffed09d8efb0539fa152812749686fb7
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-02-26 16:20:19 +1100

    add move pairs_train dir

--- backup_pairs_train.py
+++ backup_pairs_train.py
@@ -9,16 +9,21 @@ def main():
                         help='Asset directory.',
                         type=str,
                         default='/mnt/hdd/repo_results/VP-metrics-pytorch')
+    parser.add_argument('--dir_to_move',
+                        help='The dir to move.',
+                        type=str,
+                        default='pairs_train')
     parser.add_argument('--new_idx',
                         help='Move pairs_train to a new index.',
                         type=int,
-                        default='/mnt/hdd/repo_results/VP-metrics-pytorch')
+                        default=1)
     args = parser.parse_args()
     target_dirs = glob.glob(os.path.join(args.asset_dir, '*'))
     for i_dir in target_dirs:
-        old_pairs_dir = os.path.join(i_dir, 'pairs_train')
+        old_pairs_dir = os.path.join(i_dir, args.dir_to_move)
         if os.path.exists(old_pairs_dir):
-            new_pairs_dir = os.path.join(i_dir, 'pairs_train_'+str(args.new_idx))
+            new_pairs_dir = os.path.join(i_dir, 
+                    args.dir_to_move+'_'+str(args.new_idx))
             shutil.move(old_pairs_dir, new_pairs_dir)
 
 if __name__ == "__main__":
